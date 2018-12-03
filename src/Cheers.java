c9ff48179b28cc0de407ce0aebe711aeba24de3e
PeterXMR <peter.kozuch@zoho.com>
2018-12-03 14:41:13 +0100

    b3

--- src/Cheers.java
+++ src/Cheers.java
@@ -1,10 +1,13 @@
 public class Cheers {
 
     String teamName;
+    String baseLanguage;
 
     public void cheer (){
 teamName = "RedFoxes";
+baseLanguage = "english";
         System.out.println("Team name is: " +teamName);
+        System.out.println(baseLanguage);
     }
 
 }
