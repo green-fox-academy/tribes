c1e26fec84e4cf42214dfbda8a9200725cc88fd8
PeterXMR <42377209+PeterXMR@users.noreply.github.com>
2018-12-03 15:57:56 +0100

    Revert "c6"

--- src/User.java
+++ src/User.java
@@ -2,9 +2,7 @@ public class User {
 
     int age;
 
-kopsova-twoparam
-
-    public void count(int month, int age) {
+    public void count (int month, int age) {
         age = 24;
         System.out.println("Counting the countless " + month);
     }
